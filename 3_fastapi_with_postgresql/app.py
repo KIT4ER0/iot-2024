f1fdf80b6a3413a210408263275d8ed17642f457
KIT4ER0 <112687445+KIT4ER0@users.noreply.github.com>
2024-07-11 16:35:02 +0700

    Add files via upload

---
+++ 3_fastapi_with_postgresql/app.py
@@ -0,0 +1,86 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from fastapi import FastAPI, Depends, Response, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
+# Import models
+from database import SessionLocal, engine
+import models
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+router_v1 = APIRouter(prefix='/api/v1')
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# https://fastapi.tiangolo.com/tutorial/sql-databases/#crud-utils
+
+@router_v1.get('/books')
+async def get_books(db: Session = Depends(get_db)):
+    return db.query(models.Book).all()
+
+@router_v1.get('/books/{book_id}')
+async def get_book(book_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Book).filter(models.Book.id == book_id).first()
+
+@router_v1.post('/books')
+async def create_book(book: dict, response: Response, db: Session = Depends(get_db)):
+    # TODO: Add validation
+    newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'])
+    db.add(newbook)
+    db.commit()
+    db.refresh(newbook)
+    response.status_code = 201
+    return newbook
+
+@router_v1.patch('/books/{book_id}')
+async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
+    existing_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not existing_book:
+        return {
+        'message': 'Book not found'
+    }
+    if 'title' in book:
+        existing_book.title = book['title']
+    if 'author' in book:
+        existing_book.author = book['author']
+    if 'year' in book:
+        existing_book.year = book['year']
+    if 'is_published' in book:
+        existing_book.is_published = book['is_published']
+    db.commit()
+    db.refresh(existing_book)
+    return existing_book
+
+@router_v1.delete('/books/{book_id}')
+async def delete_book(book_id: int, db: Session = Depends(get_db)):
+    existing_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not existing_book:
+        return {
+        'message': 'Book not found'
+    }
+    db.delete(existing_book)
+    db.commit()
+    return {"detail": "Book deleted successfully"}
+
+app.include_router(router_v1)
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
